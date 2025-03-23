1c9da461b29911c3d5fecc7e750687891e8467b9
Dimos Theocharis <dimostheocharis10@gmail.com>
2025-03-23 17:02:20 +0200

    Added new experience item

--- src/app/Services/experience/experience.service.ts
+++ src/app/Services/experience/experience.service.ts
@@ -9,12 +9,22 @@ export class ExperienceService {
     {
       title: "Software Engineer",
       workPeriod: "November 2022 - March 2024",
-      text: "In my role as a Software Engineer in SGA i contribute in the development of informatic systems used by \
-             various greek chambers like the 'Professional Chamber of Thessaloniki'. The technlogies that i use are \
+      text: "In my role as a Software Engineer in SGA I contribute in the development of informatic systems used by \
+             various greek chambers like the 'Professional Chamber of Thessaloniki'. The technlogies that I use are \
              Angular, Nest.js, Typescript, SQL, C#, .NET and more.",
       class: "wrapper leftWrapper",
       arrowClass: "rightArrow",
       imagePath: "assets/images/sga.png"
+    },
+    {
+      title: "Software Engineer",
+      workPeriod: "March 2025 - Now",
+      text: "I contribute to the maintaining, debugging and development of AUTH's e-university services, such as \
+             the digital services for students, teachers, secretaries etc. The technologies that I use are \
+             Angular, Typescript, SQL, Postman API, Formio and more.",
+      class: "wrapper rightWrapper",
+      arrowClass: "leftArrow",
+      imagePath: "assets/images/auth.png"
     }
   ]
 
